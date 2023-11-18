7321508a22fee007cfcb217d71b3298d196d5f83
Patryk241194 <potrykus.patrykk@gmail.com>
2023-11-18 15:10:07 +0100

    Task submodule 29.1

--- src/main/java/com/crud/tasks/domain/TrelloCard.java
+++ src/main/java/com/crud/tasks/domain/TrelloCard.java
@@ -12,4 +12,25 @@ public class TrelloCard {
     private String description;
     private String pos;
     private String listId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TrelloCard that)) return false;
+
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+        if (getDescription() != null ? !getDescription().equals(that.getDescription()) : that.getDescription() != null)
+            return false;
+        if (getPos() != null ? !getPos().equals(that.getPos()) : that.getPos() != null) return false;
+        return getListId() != null ? getListId().equals(that.getListId()) : that.getListId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+        result = 31 * result + (getPos() != null ? getPos().hashCode() : 0);
+        result = 31 * result + (getListId() != null ? getListId().hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/crud/tasks/domain/TrelloList.java
+++ src/main/java/com/crud/tasks/domain/TrelloList.java
@@ -13,6 +13,23 @@ public class TrelloList {
     private String name;
     private boolean isClosed;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TrelloList that)) return false;
+
+        if (isClosed() != that.isClosed()) return false;
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        return getName() != null ? getName().equals(that.getName()) : that.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (isClosed() ? 1 : 0);
+        return result;
+    }
 }
 
 
