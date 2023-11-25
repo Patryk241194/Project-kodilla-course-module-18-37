ccd47bfd306fb15b1469fd6d92bfcd056eee05e6
Patryk241194 <128822106+Patryk241194@users.noreply.github.com>
2023-11-25 18:47:07 +0100

    Task submodule 29.3 (#13)
    * Task submodule 29.3
    
    * Task submodule 29.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,12 +5,14 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("v1/tasks")
 @CrossOrigin("*")
@@ -28,11 +30,17 @@ public class TaskController {
 
     @GetMapping(value = "{taskId}")
     public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) throws TaskNotFoundException {
-        return ResponseEntity.ok(taskMapper.mapToTaskDto(service.getTask(taskId)));
+        log.info("Fetching task with id: {}", taskId);
+        Task task = service.getTask(taskId);
+        if (task == null) {
+            throw new TaskNotFoundException();
+        }
+        return ResponseEntity.ok(taskMapper.mapToTaskDto(task));
     }
 
     @DeleteMapping(value = "{taskId}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        log.info("Deleting task with id: {}", taskId);
         service.deleteTask(taskId);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,12 +1,30 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TaskDto {
+
+    @JsonProperty("id")
     private Long id;
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("content")
     private String content;
+
+    @JsonCreator
+    public TaskDto(@JsonProperty("id") Long id,
+                   @JsonProperty("title") String title,
+                   @JsonProperty("content") String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 }
