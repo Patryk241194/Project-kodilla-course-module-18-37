fd79704e2ab27f8690c68a5c675976f715339400
Patryk241194 <potrykus.patrykk@gmail.com>
2024-08-31 18:52:09 +0200

    Thymeleaf 32.2 new features

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,7 +1,9 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -10,14 +12,33 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("goodbye_message", "Best regards");
         context.setVariable("tasks_url", "https://patryk241194.github.io/");
         context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyName);
+        context.setVariable("company_goal", companyGoal);
+        context.setVariable("company_email", companyEmail);
+        context.setVariable("company_phone", companyPhone);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
